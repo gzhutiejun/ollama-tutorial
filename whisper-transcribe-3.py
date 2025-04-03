5a475bc42aa6940f8ce6352cf14cdb11ff08a62a
Tiejun Hu <87230907+gzhutiejun@users.noreply.github.com>
2025-04-03 16:46:34 +0800

    cantonese asr support

--- whisper-transcribe-3.py
+++ whisper-transcribe-3.py
@@ -6,10 +6,12 @@ print(torch.cuda.is_available())
 def speech_to_text_pipeline(audio_file):
     try:
         # Initialize the Whisper pipeline with fallback to CPU
-        transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-small", device=-1)
+        #transcriber = pipeline("automatic-speech-recognition", model="alvanlii/whisper-small-cantonese")
+        transcriber = pipeline("automatic-speech-recognition", model="max-at-Parami/whisper-small-zh-hk")
+
 
         # Transcribe the audio file
-        result = transcriber(audio_file)
+        result = transcriber(audio_file)  # Ensure compatibility with `input_features`
         return result.get("text", "No transcription available")
     except Exception as e:
         return f"An error occurred: {e}"
